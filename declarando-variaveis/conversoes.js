5d32bc770548c0afa5db5e51750d9cacef82b875
welberthenrique <welberthenrique@gmail.com>
2022-01-12 22:10:01 -0300

    codigos de var e conversoes

---
+++ declarando-variaveis/conversoes.js
@@ -0,0 +1,14 @@
+//tipo de dados
+//booleanos
+
+//conversão implicita
+
+const numero = 456;
+const numeroString = Number('456a');
+
+console.log(numero + numeroString);
+
+//conversão explicita
+//função Number() e String()
+
+console.log(numero + numeroString);
---
+++ declarando-variaveis/script.js
@@ -0,0 +1,40 @@
+//var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area);
+// var area;
+
+//let
+//Surgiu a partir de 2015
+// let forma = 'retangulo';
+// let altura = 5;
+// let comprimento= 7;
+
+// if(forma === 'retangulo'){
+//     area = altura * comprimento;
+//     console.log(area);
+// }else{
+//     area = (altura * comprimento)/2;
+// }
+
+// console.log(area);
+
+//const
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area = 0;
+
+
+
+if(forma ==='quadrado'){
+    area = altura * comprimento;
+}else{
+    area = (altura * comprimento) /2;
+}
+
+console.log(area);
