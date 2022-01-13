8f1cc713115bf8aba3a2c40d78725ef0f30cd577
welberthenrique <welberthenrique@gmail.com>
2022-01-12 21:31:37 -0300

    re estruturando diretorios

---
+++ tipos-de-dados/type-boolean.js
@@ -0,0 +1,26 @@
+const primeiroNumero = 5;
+const segundoNumero = '5';
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
+
+
+const idade = 2;
+const desde = "12";
+
+console.log(idade + desde);
+
+
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
